cd634bf5ef372aea3dc29a6ffa12cd8c05df8255
kimuchione <tayu2849@gmail.com>
2022-12-28 02:04:25 +0900

    # how to declare implement details and syntactic sugar

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,22 @@ enum FoodType {
 
 impl Food {
     fn new(cost: u32, food_type: FoodType) -> Self {
+        //constructor method
         Self { cost, food_type }
+    }
+
+    fn print(&self) {
+        println!("It's {} won. Food type is {:#?}", self.cost, self.food_type);
+    }
+
+    fn change_type(&mut self, change_type: FoodType) {
+        self.food_type = change_type;
     }
 }
 
 fn main() {
-    let my_food = Food::new(10, FoodType::Vegi);
+    let mut my_food = Food::new(10, FoodType::Vegi);
+    my_food.print(); // == Food::print(&my_food) syntactic sugar
+    my_food.change_type(FoodType::Beef);
+    my_food.print();
 }
