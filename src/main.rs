3af291c4e9cf664d66e44d6748c9308466726575
Trevi-Lime <tayu2849@naver.com>
2022-12-28 01:43:27 +0900

    # how to declare implement and method

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
-fn main() {
-    let mut num = 0;
+#[derive(Debug)]
+struct Food {
+    cost: u32,
+    food_type: FoodType,
+}
 
-    'first_iter: while num != 5 {
-        //==tick make break point
-        num += 1;
-        println!("My number is {}", num);
-        if num == 3 {
-            'second_iter: loop {
-                num += 5;
-                break 'first_iter; //break first iteration
-            }
-        }
+#[derive(Debug)]
+enum FoodType {
+    Beef,
+    Vegi,
+    Sauce,
+}
+
+impl Food {
+    fn new(cost: u32, food_type: FoodType) -> Self {
+        Self { cost, food_type }
     }
+}
+
+fn main() {
+    let my_food = Food::new(10, FoodType::Vegi);
 }
