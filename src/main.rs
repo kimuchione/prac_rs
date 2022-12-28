273e13b891ec7ddfd88aab9ee6ae623c5fe5f34e
kimuchione <tayu2849@gmail.com>
2022-12-28 23:40:54 +0900

    # How to declare generics and fmt::display type

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,14 @@
 #[derive(Debug)]
-struct Animal {
-    age: u8,
-    name: String,
-    weight: u8,
-    is_healthy: bool,
-}
+struct Book;
 
-struct AnimalDescription {
-    name: String,
-    is_healthy: bool,
-}
-
-//destructuring
-impl AnimalDescription {
-    fn simple_initalization(input: Animal) -> Self {
-        let Animal {
-            name, is_healthy, ..
-        } = input;
-
-        Self { name, is_healthy }
-    }
+fn return_func<T: std::fmt::Display>(params: T) -> T {
+    //declare generic
+    println!("The thing is {}", params);
+    params
 }
 
 fn main() {
-    let my_pet = Animal {
-        age: 8,
-        name: "jjilong".to_string(),
-        weight: 12,
-        is_healthy: true,
-    };
-
-    //destructuring
-    let Animal {
-        age: a,
-        name: b,
-        weight: c,
-        is_healthy: d,
-    } = &my_pet;
-
-    println!(
-        "His name is {}. {} years old. and get weight {}, is healthy? {}",
-        b, a, c, d
-    );
-
-    let another_pet = AnimalDescription::simple_initalization(my_pet);
-    println!(
-        "His name is {}, is he healthy? {}",
-        another_pet.name, another_pet.is_healthy
-    );
+    let x = return_func(5);
+    let y = return_func(String::from("Hello"));
+    let z = return_func(Book); // may not compiled
 }
