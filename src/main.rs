08b3a2f0079c88f1d470b6485acdbcdc1c474f38
kimuchione <tayu2849@gmail.com>
2022-12-29 16:06:31 +0900

    # How to use where

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ use std::fmt::Display;
 #[derive(Debug)]
 struct Book;
 
-fn return_func<T: Display, U: Display + std::cmp::PartialOrd>(params: T, number1: U, number2: U) {
+fn return_func<T, U>(params: T, number1: U, number2: U)
+where
+    T: Display, // T is not keyword. If you want to write DisplayType? try it .
+    U: Display + std::cmp::PartialOrd, // If you worried about typing over you put in generics on where
+{
     let is_greater = if number1 >= number2 { true } else { false };
     println!(
         "Hey {}, Is {} greater than {} ? {} ",
