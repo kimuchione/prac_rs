8578b9cc8f19e4f2040cb14613a3a64ee376cba9
kimuchione <tayu2849@gmail.com>
2023-01-08 23:02:06 +0900

    # implement Blanket trait and confine type 2

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,48 @@
-use std::fmt::Debug;
+use std::fmt::{Debug, Display};
 
-//instance must be have a Debug type.
-trait display_state: Debug {
-    fn to_string(&self) {
+trait DisplayState {
+    //provide required type
+    fn load_state(&self)
+    where
+        Self: Debug,
+    {
         println!("status : {:?}", self);
+    }
+
+    fn load_state_with_display(&self)
+    where
+        Self: Debug + Display,
+    {
+        println!("status with display : {}", self);
     }
 }
 
+#[derive(Debug)]
 struct Jerry {}
+#[derive(Debug)]
 struct Tom {}
 
-impl<T: Debug> display_state for T {}
+impl Display for Tom {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+// <T> is free!
+impl<T> DisplayState for T {}
 
 fn main() {
     let mouse = Jerry {};
     let cat = Tom {};
 
+    mouse.load_state(); // expected Jerry
     #[cfg(panic)]
-    mouse.to_string();
-    #[cfg(panic)]
-    cat.to_string();
+    mouse.load_state_with_display();
 
     let something_string = "hello".to_string();
+    something_string.load_state(); // expected "hello"
+    something_string.load_state_with_display(); // expected hello
 
-    //instead of ToString::to_string trait
-    display_state::to_string(&something_string); // candidate #2
+    cat.load_state(); // expected Tom
+    cat.load_state_with_display(); //aborted. struct haven't attribute
 }
