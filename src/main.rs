c7ef06e48fa69ed6f4d2dae295564beb23382bee
kimuchione <tayu2849@gmail.com>
2023-01-08 22:43:27 +0900

    # implement Blanket trait and confine type

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
-trait display_state {
+use std::fmt::Debug;
+
+//instance must be have a Debug type.
+trait display_state: Debug {
     fn to_string(&self) {
-        println!("status window");
+        println!("status : {:?}", self);
     }
 }
 
 struct Jerry {}
 struct Tom {}
-//implement all type. if exist built-in function you must annotate what you use.
-impl<T> display_state for T {}
+
+impl<T: Debug> display_state for T {}
+
 fn main() {
     let mouse = Jerry {};
     let cat = Tom {};
+
+    #[cfg(panic)]
     mouse.to_string();
+    #[cfg(panic)]
     cat.to_string();
+
     let something_string = "hello".to_string();
 
     //instead of ToString::to_string trait
