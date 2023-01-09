0d4d83dc0cf511d9d15b61249078573327583d72
kimuchione <tayu2849@gmail.com>
2023-01-09 11:31:27 +0900

    # Write to Chaning function on formal in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,19 @@
-use std::fmt::{Debug, Display};
-
-trait DisplayState {
-    //provide required type
-    fn load_state(&self)
-    where
-        Self: Debug,
-    {
-        println!("status : {:?}", self);
-    }
-
-    fn load_state_with_display(&self)
-    where
-        Self: Debug + Display,
-    {
-        println!("status with display : {}", self);
-    }
-}
-
-#[derive(Debug)]
-struct Jerry {}
-#[derive(Debug)]
-struct Tom {}
-
-impl Display for Tom {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self)
-    }
-}
-
-// <T> is free!
-impl<T> DisplayState for T {}
-
 fn main() {
-    let mouse = Jerry {};
-    let cat = Tom {};
+    // 1 to 10 including 10
+    let new_vec = (1..=10).collect::<Vec<_>>();
 
-    mouse.load_state(); // expected Jerry
-    #[cfg(panic)]
-    mouse.load_state_with_display();
+    let four = new_vec.clone().into_iter().skip(1).skip(2).take(2).next();
 
-    let something_string = "hello".to_string();
-    something_string.load_state(); // expected "hello"
-    something_string.load_state_with_display(); // expected hello
+    if let Some(number) = four {
+        println!("{number}");
+    };
 
-    cat.load_state(); // expected Tom
-    cat.load_state_with_display(); //aborted. struct haven't attribute
+    let my_vec = new_vec
+        .into_iter()
+        .skip(1)
+        .skip(2)
+        .take(4)
+        .collect::<Vec<i32>>();
+
+    assert_eq!(my_vec, vec![4, 5, 6, 7]);
 }
