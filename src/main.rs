1f00f31d956aa09494f3f7c14732d611d9b92557
kimuchione <tayu2849@gmail.com>
2023-01-10 22:18:31 +0900

    # Separated by check use variables called anonymous_func or closure

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
 fn main() {
-    // 1 to 10 including 10
-    let new_vec = (1..=10).collect::<Vec<_>>();
+    let number = 9;
+    let anonymous_function = || println!("9");
+    let closure = || println!("{number}");
 
-    let four = new_vec.clone().into_iter().skip(1).skip(2).take(2).next();
-
-    if let Some(number) = four {
-        println!("{number}");
-    };
-
-    let my_vec = new_vec
-        .into_iter()
-        .skip(1)
-        .skip(2)
-        .take(4)
-        .collect::<Vec<i32>>();
-
-    assert_eq!(my_vec, vec![4, 5, 6, 7]);
+    anonymous_function();
+    closure();
 }
